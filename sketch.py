6b45dde66bbaa0dd6f32e1ab65a99bd99c53d173
lemonlemonde <mirudjun@gmail.com>
2025-02-11 23:34:32 -0800

    :sparkles: init prompt and inpainting

--- sketch.py
+++ sketch.py
@@ -1,11 +1,25 @@
 # Code Source:https://huggingface.co/docs/diffusers/using-diffusers/inpaint
 import PIL
+from PIL import Image, ImageDraw
 import requests
 import torch
 from io import BytesIO
 
 from diffusers import StableDiffusionInpaintPipeline
 
+
+# ========= Initial sketch =========
+prompt = input("Initial prompt for sketch: ")
+init_pipeline = StableDiffusionInpaintPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    torch_dtype=torch.float16,
+)
+# init_pipeline = init_pipeline.to("cuda")
+init_img = init_pipeline(prompt = prompt).images[0]
+init_img = init_img.resize((512, 512))
+init_img.show()
+
+# ========= Inpainting =========
 pipeline = StableDiffusionInpaintPipeline.from_pretrained(
     "runwayml/stable-diffusion-inpainting",
     torch_dtype=torch.float16,
@@ -14,29 +28,28 @@ pipeline = StableDiffusionInpaintPipeline.from_pretrained(
 )
 # pipeline = pipeline.to("cuda")
 
+# ========== User provides mask ==========
+# TODO: make this interactive with UI
+topleft_x, topleft_y = input("Top left corner of mask (x1, y1): ").split(", ")
+bottomright_x, bottomright_y = input("Bottom right corner of mask (x2, y2): ").split(", ")
+coords = [(int(topleft_x), int(topleft_y)), (int(bottomright_x), int(bottomright_y))]
+# display the mask
+draw = ImageDraw.Draw(init_img)
+draw.rectangle(coords, outline="red", width=5)
+init_img.show()
 
-# Code Source: https://huggingface.co/docs/diffusers/using-diffusers/inpaint
-def download_image(url):
-    response = requests.get(url)
-    return PIL.Image.open(BytesIO(response.content)).convert("RGB")
+# create mask
+# black background
+mask_img = Image.new("1", (512, 512))
+mask_draw = ImageDraw.Draw(mask_img)
+mask_draw.rectangle((512,512), fill="#000000")
+# white mask
+mask_draw.rectangle(coords, fill="#ffffff")
+mask_img.show()
 
-img_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/billow926-12-Wc-Zgx6Y.png"
-mask_url = "https://raw.githubusercontent.com/CompVis/latent-diffusion/main/data/inpainting_examples/billow926-12-Wc-Zgx6Y_mask.png"
+prompt = input("What would you like to change? ")
 
-init_image = download_image(img_url).resize((512, 512))
-mask_image = download_image(mask_url).resize((512, 512))
+# generate inpainted image
+new_image = pipeline(prompt=prompt, image=init_img, mask_image=mask_img).images[0]
 
-
-prompt = "Two puppies wearing red hats"
-
-new_image = pipeline(prompt=prompt, image=init_image, mask_image=mask_image).images[0]
-
-
-new_image
-
-# from diffusers import DiffusionPipeline
-
-# pipe = DiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-inpainting")
-
-# prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
-# image = pipe(prompt).images[0]
+new_image.show()
